4c9ebb87a26c8cffd0e45a1b7189d80eb0fb2811
myles800 <50370395+myles800@users.noreply.github.com>
2020-10-07 23:43:05 +0200

    PasswordHashVerifier.js

--- src/PasswordHashVerifier.js
+++ src/PasswordHashVerifier.js
@@ -1,3 +1,18 @@
+const nacl = require('libsodium-wrappers')
 module.exports = async () => {
-   
+await nacl.ready;
+    
+    let sodium = nacl;
+    return Object.freeze({
+        verify: (hashedPw,Pw)=> {
+            if(!hashedPw){
+                throw 'no hashed password';
+            }
+            if(!Pw){
+                throw 'no password';
+            }
+            return sodium.crypto_pwhash_str_verify(hashedPw,Pw);
+        }
+  
+    })
 }
